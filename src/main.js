df1ca134bfab6ca26fab487cf848feff4e2a74b5
Mr_Long <1169347662@qq.com>
2021-05-13 21:05:51 +0800

    '图片懒加载'

--- src/main.js
+++ src/main.js
@@ -1,19 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
-
 import store from "@/store/store";
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
+//添加事件总线
 Vue.prototype.$bus = new Vue()  //Vue实例可以作为事件总线 (可以发送监听事件)
 //安装toast插件
 Vue.use(toast)
+//解决移动端300ms延迟
+FastClick.attach(document.body)
+//使用懒加载插件 Vue.use(VueLazyLoad, 占位图)
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
+
 
 new Vue({
   render: h => h(App),
   router,
   store,
-  toast
 }).$mount('#app')
